03ff4ab5aecf453752a76ce48c5d1b4bca472304
Wycliffe691 <122208584+Wycliffe691@users.noreply.github.com>
2023-03-12 11:41:49 +0300

    Update portfolio.js
    I have added JavaScript to my website.

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,18 @@
+const bar = document.querySelector('.bar');
+const navLinks = document.querySelector('.nav-links');
+
+bar.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+// Remove the show class from nav-links by default
+navLinks.classList.remove('show');
+
+// Add click event listeners to each navigation link to hide nav-links when clicked
+const navLinksList = document.querySelectorAll('.nav-links a');
+navLinksList.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
 
